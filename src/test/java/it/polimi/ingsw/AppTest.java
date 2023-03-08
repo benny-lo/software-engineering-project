b80f9a4166500d4a14983506d41ab01cbc509e59
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-03-08 20:10:06 +0100

    initial commit

---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void theAnswer()
+    {
+        assertEquals(42, 22+20);
+    }
+}
