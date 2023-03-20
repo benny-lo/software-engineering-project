96cd577a0a0b3a71bba33774be90d09e41fe1fab
Pietro <pietromassari@icloud.com>
2023-03-20 13:15:37 +0100

    add CommonGoalPattern6,CommonGoalPattern7,fixed CommonG

--- src/main/java/it/polimi/ingsw/model/board/commonGoalCard/CommonGoalPattern6.java
+++ src/main/java/it/polimi/ingsw/model/board/commonGoalCard/CommonGoalPattern6.java
@@ -1,31 +1,31 @@
 package it.polimi.ingsw.model.board.commonGoalCard;
 
-import it.polimi.ingsw.model.Item;
 import it.polimi.ingsw.model.player.Bookshelf;
+import it.polimi.ingsw.model.Item;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class CommonGoalPattern5 implements CommonGoalPatternInterface{
+public class CommonGoalPattern6 implements CommonGoalPatternInterface{
     @Override
+
     public boolean check(Bookshelf bookshelf)
     {
         int groups = 0;
         Set<Item> set = new HashSet<>();
-        for(int i=0;i<bookshelf.getColumns()-1;i++)
+        for(int i=0;i<bookshelf.getRows();i++)
         {
             set.clear();
-            for(int j=0;j<bookshelf.getRows()-1;j++)
+            for(int j=0;j<bookshelf.getColumns();j++)
             {
-                set.add(bookshelf.tileAt(j,i));
+                set.add(bookshelf.tileAt(i,j));
             }
-            if(set.size() <= 3)
+            if(set.size() == 5)
             {
                 groups++;
             }
         }
-
-        return groups >= 3;
+        return groups >= 2;
 
     }
 }
--- src/main/java/it/polimi/ingsw/model/board/commonGoalCard/CommonGoalPattern7.java
+++ src/main/java/it/polimi/ingsw/model/board/commonGoalCard/CommonGoalPattern7.java
@@ -1,23 +1,25 @@
 package it.polimi.ingsw.model.board.commonGoalCard;
 
-import it.polimi.ingsw.model.Item;
 import it.polimi.ingsw.model.player.Bookshelf;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class CommonGoalPattern5 implements CommonGoalPatternInterface{
+import it.polimi.ingsw.model.Item;
+
+public class CommonGoalPattern7 implements CommonGoalPatternInterface{
     @Override
+
     public boolean check(Bookshelf bookshelf)
     {
-        int groups = 0;
         Set<Item> set = new HashSet<>();
-        for(int i=0;i<bookshelf.getColumns()-1;i++)
+        int groups = 0;
+        for(int i=0;i<bookshelf.getRows();i++)
         {
             set.clear();
-            for(int j=0;j<bookshelf.getRows()-1;j++)
+            for(int j=0;j<bookshelf.getColumns();j++)
             {
-                set.add(bookshelf.tileAt(j,i));
+                set.add(bookshelf.tileAt(i,j));
             }
             if(set.size() <= 3)
             {
@@ -25,7 +27,6 @@ public class CommonGoalPattern5 implements CommonGoalPatternInterface{
             }
         }
 
-        return groups >= 3;
-
+        return groups >= 4;
     }
 }
