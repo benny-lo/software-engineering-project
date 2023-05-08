6998f3e00ddab72705da3121391c5a566ac0c853
Andandreigon <nbomben01@gmail.com>
2023-05-08 16:35:40 +0200

    View interface update

--- src/main/java/it/polimi/ingsw/view/ViewInterface.java
+++ src/main/java/it/polimi/ingsw/view/ViewInterface.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw.view;
 
+import it.polimi.ingsw.model.Item;
+import it.polimi.ingsw.model.chat.Message;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ViewInterface {
-    // introdurre i vari tipi di update che la view può ricevere.
+    public void printGamesList();
+    public void printAcceptedAction(boolean isAccepted);
+    public void printBookshelves(Map<String, Item[][]> bookshelves);
+    public void printLivingroom(Item[][] livingroom);
+    public void printWaitingUpdate(int numberMissingPlayers);
+    public void printCommonGoalCard(List<int[]> commonGoalCards);
+    public void printPersonalGoalCard(int personalGoalCard);
+    public void printChat(List<Message> chat);
+    public void printStartTurn(String username);
+    public void printEndGame();
+    public void printTileSelectionResponse(boolean response);
+    public void printScores(Map<String, Integer> scores);
 }
