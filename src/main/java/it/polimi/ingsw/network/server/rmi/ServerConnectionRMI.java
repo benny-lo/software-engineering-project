ac22fdb9a986587c5b1312125c14db41c91f28b0
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-05-23 15:26:25 +0200

    update rmi from server

--- src/main/java/it/polimi/ingsw/network/server/rmi/ServerConnectionRMI.java
+++ src/main/java/it/polimi/ingsw/network/server/rmi/ServerConnectionRMI.java
@@ -3,16 +3,20 @@ package it.polimi.ingsw.network.server.rmi;
 import it.polimi.ingsw.network.client.rmi.ClientConnectionRMIInterface;
 import it.polimi.ingsw.network.server.ServerConnection;
 import it.polimi.ingsw.utils.message.Beep;
+import it.polimi.ingsw.utils.message.Message;
 import it.polimi.ingsw.utils.message.client.*;
 import it.polimi.ingsw.utils.message.server.*;
 import it.polimi.ingsw.view.server.ServerInputViewInterface;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ServerConnectionRMI extends UnicastRemoteObject implements ServerConnection, ServerConnectionRMIInterface {
+    private final Queue<Message> sendingQueue;
     private ServerInputViewInterface receiver;
     private final ClientConnectionRMIInterface client;
     private final Timer serverTimer;
@@ -22,18 +26,19 @@ public class ServerConnectionRMI extends UnicastRemoteObject implements ServerCo
 
     public ServerConnectionRMI(ClientConnectionRMIInterface client) throws RemoteException {
         super();
+        this.sendingQueue = new ArrayDeque<>();
         this.client = client;
         this.serverTimer = new Timer();
         this.clientTimer = new Timer();
         this.beepLock = new Object();
     }
 
-    public void startTimers() {
+    public void start() {
         serverTimer.schedule(new TimerTask() {
             @Override
             public void run() {
                 try {
-                    client.beep(new Beep());
+                    client.receive(new Beep());
                 } catch (RemoteException e) {
                     receiver.disconnect();
                 }
@@ -52,6 +57,52 @@ public class ServerConnectionRMI extends UnicastRemoteObject implements ServerCo
                 receiver.disconnect();
             }
         }, 2000, 2000);
+
+        (new Thread(() -> {
+            while(true) {
+                synchronized (sendingQueue) {
+                    Message message = sendingQueue.poll();
+                    if (message == null) continue;
+                    try {
+                        if (message instanceof LivingRoomUpdate) {
+                            client.receive((LivingRoomUpdate) message);
+                        } else if (message instanceof BookshelfUpdate) {
+                            client.receive((BookshelfUpdate) message);
+                        } else if (message instanceof WaitingUpdate) {
+                            client.receive((WaitingUpdate) message);
+                        } else if (message instanceof ScoresUpdate) {
+                            client.receive((ScoresUpdate) message);
+                        } else if (message instanceof EndingTokenUpdate) {
+                            client.receive((EndingTokenUpdate) message);
+                        } else if (message instanceof CommonGoalCardsUpdate) {
+                            client.receive((CommonGoalCardsUpdate) message);
+                        } else if (message instanceof PersonalGoalCardUpdate) {
+                            client.receive((PersonalGoalCardUpdate) message);
+                        } else if (message instanceof ChatUpdate) {
+                            client.receive((ChatUpdate) message);
+                        } else if (message instanceof StartTurnUpdate) {
+                            client.receive((StartTurnUpdate) message);
+                        } else if (message instanceof EndGameUpdate) {
+                            client.receive((EndGameUpdate) message);
+                        } else if (message instanceof GamesList) {
+                            client.receive((GamesList) message);
+                        } else if (message instanceof ItemsSelected) {
+                            client.receive((ItemsSelected) message);
+                        } else if (message instanceof GameData) {
+                            client.receive((GameData) message);
+                        } else if (message instanceof AcceptedInsertion) {
+                            client.receive((AcceptedInsertion) message);
+                        } else if (message instanceof ChatAccepted) {
+                            client.receive((ChatAccepted) message);
+                        }
+                    } catch (RemoteException e) {
+                        serverTimer.cancel();
+                        clientTimer.cancel();
+                        receiver.disconnect();
+                    }
+                }
+            }
+        })).start();
     }
 
     @Override
@@ -98,140 +149,92 @@ public class ServerConnectionRMI extends UnicastRemoteObject implements ServerCo
 
     @Override
     public void send(LivingRoomUpdate update) {
-        try {
-            client.sendLivingRoomUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(BookshelfUpdate update) {
-        try {
-            client.sendBookshelfUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(WaitingUpdate update) {
-        try {
-            client.sendWaitingUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(ScoresUpdate update) {
-        try {
-            client.sendScoresUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(EndingTokenUpdate update) {
-        try {
-            client.sendEndingTokenUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(CommonGoalCardsUpdate update) {
-        try {
-            client.sendCommonGoalCardUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(PersonalGoalCardUpdate update) {
-        try {
-            client.sendPersonalGoalCardUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(ChatUpdate update) {
-        try {
-            client.sendChatUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(StartTurnUpdate update) {
-        try {
-            client.sendStartTurnUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(EndGameUpdate update) {
-        try {
-            client.sendEndGameUpdate(update);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(update);
         }
     }
 
     @Override
     public void send(GamesList gamesList) {
-        try {
-            client.sendListOfGames(gamesList);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(gamesList);
         }
     }
 
     @Override
     public void send(ItemsSelected itemsSelected) {
-        try {
-            client.sendItemsSelected(itemsSelected);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(itemsSelected);
         }
     }
 
     @Override
     public void send(GameData gameData) {
         try {
-            client.sendGameDimensions(gameData);
+            client.receive(gameData);
         } catch (RemoteException e) {
             serverTimer.cancel();
             clientTimer.cancel();
@@ -241,23 +244,15 @@ public class ServerConnectionRMI extends UnicastRemoteObject implements ServerCo
 
     @Override
     public void send(AcceptedInsertion acceptedInsertion) {
-        try {
-            client.sendAcceptedInsertion(acceptedInsertion);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(acceptedInsertion);
         }
     }
 
     @Override
     public void send(ChatAccepted chatAccepted) {
-        try {
-            client.sendChatAccepted(chatAccepted);
-        } catch (RemoteException e) {
-            serverTimer.cancel();
-            clientTimer.cancel();
-            receiver.disconnect();
+        synchronized (sendingQueue) {
+            sendingQueue.add(chatAccepted);
         }
     }
 }
