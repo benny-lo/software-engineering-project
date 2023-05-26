b3f659c0b2a26c9973582ea257868658a71e653a
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-05-26 17:27:59 +0200

    interface ServerUpdateViewInterface

---
+++ src/main/java/it/polimi/ingsw/view/server/ServerUpdateViewInterface.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.view.server;
+
+import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.view.UpdateViewInterface;
+
+public interface ServerUpdateViewInterface extends UpdateViewInterface {
+    String getNickname();
+    void setNickname(String nickname);
+    void setController(Controller controller);
+    boolean isLoggedIn();
+    boolean isInGame();
+}
