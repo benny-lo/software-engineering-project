437ba96947cbc01ac5042045a114b567d3a8bf12
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-06-12 16:47:40 +0200

    updated javadoc

--- src/main/java/it/polimi/ingsw/network/server/rmi/ConnectionEstablishmentRMI.java
+++ src/main/java/it/polimi/ingsw/network/server/rmi/ConnectionEstablishmentRMI.java
@@ -5,7 +5,18 @@ import it.polimi.ingsw.network.client.rmi.ClientConnectionRMIInterface;
 
 import java.rmi.RemoteException;
 
+/**
+ * Class implementing the {@code ConnectionEstablishmentRMIInterface}.
+ */
 public class ConnectionEstablishmentRMI implements ConnectionEstablishmentRMIInterface {
+    /**
+     * {@inheritDoc}
+     * Moreover, it creates the {@code ServerConnectionRMI} object (and starts its timer) and
+     * the {@code VirtualView} representing the connected client in the server.
+     * @param clientConnectionRMIInterface rmi interface of the client.
+     * @return {@code ServerConnectionRMIInterface} interface representing the connection to the server.
+     * @throws RemoteException rmi exception.
+     */
     @Override
     public ServerConnectionRMIInterface init(ClientConnectionRMIInterface clientConnectionRMIInterface) throws RemoteException {
         ServerConnectionRMI serverConnection = new ServerConnectionRMI(clientConnectionRMIInterface);
--- src/main/java/it/polimi/ingsw/network/server/rmi/ConnectionEstablishmentRMIInterface.java
+++ src/main/java/it/polimi/ingsw/network/server/rmi/ConnectionEstablishmentRMIInterface.java
@@ -5,6 +5,15 @@ import it.polimi.ingsw.network.client.rmi.ClientConnectionRMIInterface;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Interface to export through rmi. It allows the client to start a rmi connection to the server.
+ */
 public interface ConnectionEstablishmentRMIInterface extends Remote {
+    /**
+     * Initializes the connection to server.
+     * @param clientConnectionRMIInterface rmi interface of the client.
+     * @return {@code ServerConnectionRMIInterface} interface representing the connection to the server.
+     * @throws RemoteException rmi exception.
+     */
     ServerConnectionRMIInterface init(ClientConnectionRMIInterface clientConnectionRMIInterface) throws RemoteException;
 }
