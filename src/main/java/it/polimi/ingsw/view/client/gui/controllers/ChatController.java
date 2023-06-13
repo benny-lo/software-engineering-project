cdea4d9fe94dc1393b86cfcef6f22bd419b11b68
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-06-13 17:40:46 +0200

    fixed bug in chat: wrong messages displayed.

--- src/main/java/it/polimi/ingsw/view/client/gui/controllers/ChatController.java
+++ src/main/java/it/polimi/ingsw/view/client/gui/controllers/ChatController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
+    private final static String BROADCAST = "all";
     private static GUInterface guInterface;
     @FXML
     private ComboBox<String> playerMenu;
@@ -66,8 +67,13 @@ public class ChatController implements Initializable {
     }
 
     private boolean isToDisplay(ChatUpdate message) {
+        if (isBroadcast(message)) return BROADCAST.equals(playerMenu.getValue());
         return playerMenu.getValue().equals(message.getSender()) ||
                 playerMenu.getValue().equals(message.getReceiver());
+    }
+
+    private boolean isBroadcast(ChatUpdate message) {
+        return BROADCAST.equals(message.getReceiver()) || BROADCAST.equals(message.getSender());
     }
 
     private Label createLabel(ChatUpdate message) {
