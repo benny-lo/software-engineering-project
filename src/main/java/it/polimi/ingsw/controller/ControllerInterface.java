213b26eebba0ec4ec7cc4953a16ead991279a83b
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-06-25 18:38:06 +0200

    deleted utils.action package

--- src/main/java/it/polimi/ingsw/controller/ControllerInterface.java
+++ src/main/java/it/polimi/ingsw/controller/ControllerInterface.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.controller;
 
-import it.polimi.ingsw.utils.action.ActionListener;
+import it.polimi.ingsw.utils.game.Position;
+import it.polimi.ingsw.view.server.ServerUpdateViewInterface;
 
-public interface ControllerInterface extends ActionListener {
+import java.util.List;
+
+public interface ControllerInterface {
+    void join(ServerUpdateViewInterface view);
+    void livingRoom(List<Position> positions, ServerUpdateViewInterface view);
+    void bookshelf(int column, List<Integer> permutation, ServerUpdateViewInterface view);
+    void chat(String text, String receiver, ServerUpdateViewInterface view);
+    void disconnection(ServerUpdateViewInterface view);
     boolean isStarted();
     int getNumberPlayers();
     int getNumberCommonGoalCards();
