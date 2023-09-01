45d0d591566c8ef016945dc861439998e9c2cfaa
benny-lo <lorenzo2.benedetti@mail.polimi.it>
2023-09-01 19:28:36 +0200

    fixed ChatListener; Controller now sends the old chat messages to a reconnecting player.

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -318,6 +318,11 @@ public class Controller implements ControllerInterface {
             v.onDisconnectionUpdate(disconnection);
         }
 
+        System.out.println(chatListener.getChatUpdates(nickname));
+        for(ChatUpdate c : chatListener.getChatUpdates(nickname)) {
+            v.onChatUpdate(c);
+        }
+
         notifyScoresToEverybody();
 
         if (!onHold) {
